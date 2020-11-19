f9dbcf3df11b5cfa7c5a13f4817682e825127cf5
tetsuji <tetsubfg@gmail.com>
2020-11-19 14:14:30 +0900

    add laravel10 task

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('admin/profile/create', 'Admin\ProfileController@create');
+    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+});
+
+Route::get('XXX', 'AAAController@bbb');
