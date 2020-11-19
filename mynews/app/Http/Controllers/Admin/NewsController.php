d721d76c0e776022d9411b92a8fbe206e59a2c43
tetsuji <tetsubfg@gmail.com>
2020-11-19 15:08:23 +0900

    NewsController bugfix

--- mynews/app/Http/Controllers/Admin/NewsController.php
+++ mynews/app/Http/Controllers/Admin/NewsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class NwesController extends Controller
+class NewsController extends Controller
 {
     //
     public function add()
