96625613cf187ff956e860d62168194e33c917ea
tetsuji <tetsubfg@gmail.com>
2020-11-23 12:36:39 +0900

    add laravel17 tasks

--- mynews/app/Http/Controllers/Admin/ProfileController.php
+++ mynews/app/Http/Controllers/Admin/ProfileController.php
@@ -19,26 +19,42 @@ class ProfileController extends Controller
         // varidationを行う
         $this->validate($request, Profile::$rules);
 
-        $profiles = new Profile;
+        $profile = new Profile;
         $form = $request->all();
 
         // フォームから送信されてきた_tokenを削除する
         unset($form['_token']);
 
         // データベースに保存する
-        $profiles->fill($form);
-        $profiles->save();
+        $profile->fill($form);
+        $profile->save();
 
-        return redirect('admin/profile/create');
+        return redirect('admin/profile/edit');
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if(empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
-    public function update()
+    public function update(Request $request)
     {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
         return redirect('admin/profile/edit');
     }
 }
--- mynews/resources/views/admin/profile/edit.blade.php
+++ mynews/resources/views/admin/profile/edit.blade.php
@@ -2,16 +2,62 @@
 @extends('layouts.admin')
 
 
-{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+{{-- profile.blade.phpの@yield('title')に'プロフィールの編集画面'を埋め込む --}}
 @section('title', 'プロフィールの編集画面')
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
-      <div class="row">
-        <div class="col-md-8 mx-auto">
-          <h2>プロフィールの編集画面</h2>
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールの編集画面</h2>
+
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($error->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10">
+                            <select class="form-control" name="gender" value="{{ $profile_form->gender }} ">
+                            <option>男性</option>
+                            <option>女性</option>
+                            <option>教えない</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介文</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="登録">
+                        </div>
+                    </div>
+
+                </form>
+            </div>
         </div>
-      </div>
     </div>
 @endsection
